4496f6be57d861716030daa13d4e0fbaaf5f4e29
Rohith Venkat <rohithv97@outlook.com>
2021-02-21 20:08:45 +0530

    npm update

--- mean-course/backend/routes/user.js
+++ mean-course/backend/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 
 const User = require("../models/user");
+const { request } = require("node:http");
 
 const router = express.Router();
 
@@ -22,6 +23,31 @@ router.post("/signup", (request, response, next) => {
       .catch((error) => {
         response.status(500).json({
           error: error,
+        });
+      });
+  });
+});
+
+router.post("/login", (request, response, next) => {
+  User.findOne({
+    email: request.body.email,
+  }).then((user) => {
+    if (!user) {
+      return response.status(401).json({
+        message: "Email not found",
+      });
+    }
+    return bcrypt
+      .compare(request.body.password, user.password)
+      .then((result) => {
+        if (!result) {
+          return response.status(401).json({
+            message: "Email not found",
+          });
+      })
+      .catch((exception) => {
+        return response.status(401).json({
+          message: "Error in Password comparison",
         });
       });
   });
